dd2c0d08b1a42f21c7307993ae276a8ffb36e8a5
Silja Ojanen <44512829+Ronttikasa@users.noreply.github.com>
2022-12-21 01:20:59 +0200

    Create invoke tasks

---
+++ tasks.py
@@ -0,0 +1,13 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/wsgi.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
