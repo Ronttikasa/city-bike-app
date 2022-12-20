a013c2a93ad7f3aad5b39bf34c00e4561beadc6d
Silja Ojanen <44512829+Ronttikasa@users.noreply.github.com>
2022-12-21 01:26:26 +0200

    Fetch and show a list of journeys

--- src/application/repositories/city_bike_repository.py
+++ src/application/repositories/city_bike_repository.py
@@ -10,6 +10,15 @@ class CityBikeRepository:
         sql = """SELECT "FID", "ID", "Nimi", "Osoite", "Kaupunki" FROM stations"""
         return self.db.session.execute(sql).fetchall()
 
+    def get_journeys(self):
+        sql = """SELECT j."Departure", j."Return", s1."Nimi" AS "departure_station", s2."Nimi" AS "return_station", j."Distance", j."Duration"
+            FROM journeys as j
+            JOIN stations as s1 ON j."Departure station id"=s1."ID"
+            JOIN stations as s2 ON j."Return station id"=s2."ID"
+            LIMIT 10
+            """
+        return self.db.session.execute(sql).fetchall()
+
 citybike_repo = CityBikeRepository()
 
         
--- src/application/routes.py
+++ src/application/routes.py
@@ -2,7 +2,7 @@ from flask import request, make_response, render_template
 from flask import current_app as app
 from . import db
 from .services.import_stations import import_stations
-from .services.import_journeys import import_journeys
+from .services.journey_service import journey_service
 from .repositories.city_bike_repository import citybike_repo
 
 @app.route('/', methods=['GET'])
@@ -15,6 +15,11 @@ def index():
 def add_journeys():
     files = ["2021-05.csv", "2021-06.csv", "2021-07.csv"]
     for file in files:
-        import_journeys(file)
+        journey_service.import_journeys(file)
     return make_response("Hurraa!")
+
+@app.route("/journeys", methods=['GET'])
+def journeys():
+    journeys = journey_service.show_journeys()
+    return render_template("journeys.html", journeys=journeys)
 
