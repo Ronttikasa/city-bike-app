89865b825ce96a7bfeec7aec0cd916c2e61c466b
Silja Ojanen <44512829+Ronttikasa@users.noreply.github.com>
2023-02-01 19:32:22 +0200

    Add navigation

--- src/application/routes.py
+++ src/application/routes.py
@@ -1,21 +1,20 @@
-from flask import request, make_response, render_template
+from flask import make_response, render_template, redirect
 from flask import current_app as app
 from .services.import_stations import import_stations
 from .services.journey_service import journey_service
 from .repositories.city_bike_repository import citybike_repo
 
-@app.route("/", methods=["GET"])
-def index():
+@app.route("/add-stations", methods=["GET"])
+def add_stations():
     import_stations()
-    stations = citybike_repo.get_all_stations()
-    return render_template("index.html", stations=stations)
+    return redirect("/stations")
 
 @app.route("/add-journeys", methods=["GET"])
 def add_journeys():
     files = ["2021-05.csv", "2021-06.csv", "2021-07.csv"]
     for file in files:
         journey_service.import_journeys(file)
-    return make_response("Hurraa!")
+    return redirect("/journeys")
 
 @app.route("/journeys", methods=["GET"])
 def journeys():
@@ -26,3 +25,12 @@ def journeys():
 def station(id):
     station = journey_service.get_station_data(id)
     return render_template("station.html", **station)
+
+@app.route("/stations", methods=["GET"])
+def stations():
+    stations = citybike_repo.get_all_stations()
+    return render_template("index.html", stations=stations)
+
+@app.route("/", methods=["GET"])
+def index():
+    return redirect("/stations")     
